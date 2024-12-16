d0ade2bf60144762183ce40663b57bddf5022b9e
Mingxuan Che <mxche2019@outlook.com>
2024-12-16 16:39:59 +0100

    save braitenberg

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -15,27 +15,37 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
 
     height, width = shape
     half_width = width // 2
+    w_margin = 100
     half_height = height // 2
     # strategy 1 (too aggresive)
-    # # left half all positive
-    # res[forward_dist:, :half_width] = 1
-    # # right half all negative
-    # res[forward_dist:, half_width:] = -1
+    forward_dist = half_height -100
+    # left half all positive
+    # res = np.ones(shape=shape, dtype="float32")
+    
+    res[forward_dist:, :half_width] = 1
+    # right half all negative
+    res[forward_dist:, half_width:] = -1
+
+    res[:, :w_margin] = 0
+    res[:, width - w_margin: width] = 0
+
+    res[0:forward_dist, :half_width] = -1
+    res[0:forward_dist, half_width:width] = 1
     
     # strategy 2 
     # use a triangle shape for both left half and right half
     # bottom of the image (large height value) is the longer side
     
-    scaling = 2.5
-    for i in range(half_height, height):
-        num_filling =(half_width - (i-half_height)* scaling) 
-        num_filling = np.clip(num_filling, 0, half_width).astype(int)
-        res[i, num_filling:half_width] = 1
+    # scaling = 2
+    # for i in range(half_height, height):
+    #     num_filling =(half_width - (i-half_height)* scaling) 
+    #     num_filling = np.clip(num_filling, 0, half_width).astype(int)
+    #     res[i, num_filling:half_width] = 1
     
-    for i in range(half_height, height):
-        num_filling = (i-half_height) * scaling
-        num_filling = np.clip(num_filling, 0, half_width).astype(int)
-        res[i, half_width:half_width+num_filling] = -1
+    # for i in range(half_height, height):
+    #     num_filling = (i-half_height) * scaling
+    #     num_filling = np.clip(num_filling, 0, half_width).astype(int)
+    #     res[i, half_width:half_width+num_filling] = -1
     
     # # patch bottom center a square to avoid the robot stuck in the middle
     # # patch_height = 200
@@ -44,11 +54,11 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # res[height-patch_height:, half_width-patch_width:half_width+patch_width] = -1
     
     # patch the top to let the robot move forward
-    forward_dist = 150
-    patch_dist = 75
+    # forward_dist = 150
+    # patch_dist = 75
 
-    res[:forward_dist, :patch_dist] = -1
-    res[:forward_dist, width - patch_dist:] = 1
+    # res[:forward_dist, :patch_dist] = -1
+    # res[:forward_dist, width - patch_dist:] = 1
     
 
     return res
